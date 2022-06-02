06c6d9ad0dfce44a871b9ce0e440c5fddd674efb
ofemjohn <ofemjohn@gmail.com>
2022-06-02 05:13:41 -0700

    writing a c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *main - print a text 
+  *
+  * return a value: 0;
+  */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle"n\");
+	return (0);
+}
