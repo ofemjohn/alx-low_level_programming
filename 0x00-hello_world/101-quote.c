665086c3752720699380efea29c61d8ee739e0eb
ofemjohn <ofemjohn@gmail.com>
2022-06-02 14:16:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,13 @@
  **/
 
 int main(void)
+
 {
 char phrase[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 write(2, "and that piece of art is useful\" - Dora Korpar,
 2015-10-19\n",	sizeof(phrase));
+
 return (1);
 
 }
