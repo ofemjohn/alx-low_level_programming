44f078c43b18002422c0c7bd4209426049cd6223
ofemjohn <ofemjohn@gmail.com>
2022-06-02 12:45:20 -0700

    print a c program followed with a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - returns 0
+ *
+ *
+ *
+ *Description - main returns 0
+ *
+ *printf - Prints the string to stdout
+ *
+ *Return: 0
+ *
+ **/
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
