f9a3645878ee9a5e4b5c04d321d48dc1bac8dbcf
ofemjohn <ofemjohn@gmail.com>
2022-06-03 12:49:07 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,23 +2,30 @@
 
 
 /**
- * main - Entry point
+ *main - Prints numbers between 0 to 9.
  *
  *Return: Always 0 (Success)
  *
-*/
+ */
 
 int main(void)
 
 {
 
-	int num;
 
-	for (num = 0; num < 10; num++)
 
-	printf("%d", num);
+	int i;
 
-	putchar ('\n');
+
+	for (i = 48; i < 58; i++)
+
+	{
+
+	putchar(i);
+
+	}
+
+	putchar('\n');
 
 	return (0);
 
