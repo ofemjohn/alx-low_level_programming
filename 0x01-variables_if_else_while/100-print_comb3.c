d6c333a0a29652fdb261f738d38b95aa7902729e
ofemjohn <ofemjohn@gmail.com>
2022-06-03 15:41:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,13 +14,15 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-	for (digit2 = digit + 1; digit2 < 10; digit2++)
+	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 	{
 	putchar((digit1 % 10) + '0');
 	putchar((digit2 % 10) + '0');
 
 	if (digit1 == 8 && digit2 == 9)
-	continue
+
+	continue;
+
 
 	putchar(',');
 	putchar(' ');
