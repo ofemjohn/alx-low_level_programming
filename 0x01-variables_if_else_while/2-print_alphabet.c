0525fd23c4006e34b5759c8b033935abd1d54ff5
ofemjohn <ofemjohn@gmail.com>
2022-06-03 10:51:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio. h>
+
+/**
+ *main - print the alphabet
+ *
+ *return: Always 0 (success)
+ */
+
+int main(void)
+{
+		char abc;
+		for (abc = 'a'; abc <= 'z'; abc++)
+		putchar (abc);
+		putchar ('\n');
+		
+		return (0);
+
+}
