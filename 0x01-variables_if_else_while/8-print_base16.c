bfae589d37793e6aeec5d0103be725a00abec3ca
ofemjohn <ofemjohn@gmail.com>
2022-06-03 13:16:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char abc;
+
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar (num + '0');
+	}
+
+	for (abc = 'a'; abc <= 'f'; abc++)
+	{
+		putchar (abc);
+	}
+	putchar ('\n');
+
+	return (0);
+
+}
