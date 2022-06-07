318ff5f52d76ae5e5aeb9a8202790eaca791bfbf
ofemjohn <ofemjohn@gmail.com>
2022-06-07 14:00:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+nclude "holberton.h"
+
+/**
+ * print_alphabet_x10- prints the alphabet 10x
+ *
+ * Return ()
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int counter;
+	int x;
+
+	while (x < 10)
+	{
+		for (counter = 0; counter < 26; counter++)
+		{
+			_putchar(alphabet[counter]);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
+
