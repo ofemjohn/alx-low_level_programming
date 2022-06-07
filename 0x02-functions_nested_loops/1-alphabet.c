ab6e6bf1a26b4499424f18a3adb0a52cee57de51
ofemjohn <ofemjohn@gmail.com>
2022-06-07 13:53:18 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints the lowercase alphabet
+ *
+ * Return- void
+ */
+
+void print_alphabet(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int x;
+
+	for (x = 0; x < 26; x++)
+	{
+		_putchar(alphabet[x]);
+
+	}
+
+	_putchar('\n');
+
+}
