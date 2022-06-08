f334cf04835b1b9384ed70d416aa20bef9117c6c
ofemjohn <ofemjohn@gmail.com>
2022-06-08 01:31:50 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check the code for Holberton School students.
+ *
+ * @a: An ascii char to compare
+ *
+ * @b: An ascii char to compare
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
