5ab9f417cc45527a579109d520d32ba562336063
ofemjohn <ofemjohn@gmail.com>
2022-06-08 03:55:55 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - print time table.
+ * @n: integer.
+ * Return:void.
+ */
+void print_times_table(int n)
+{
+	int row, col, tmp;
+
+	if ((n < 0) || (n > 15))
+		return;
+	for (row = 0; row <= n; row++)
+	{
+		tmp = row;
+		_putchar('0');
+		for (col = 0; col < n; col++)
+		{
+			if (tmp < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(tmp + '0');
+			}
+			else if (tmp < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((tmp / 10) + '0');
+				_putchar((tmp % 10) + '0')
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((tmp / 100) + '0');
+				_putchar((tmp / 10) % 10 + '0');
+				_putchar((tmp % 10) + '0');
+			}
+			tmp += row;
+		}
+		_putchar('\n');
+	}
+}
+
