faf220503c0d6022ff306584d51544b4d125c943
ofemjohn <ofemjohn@gmail.com>
2022-06-08 04:04:01 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - count multiple.
+ *
+ * Return:void
+ */
+int main(void)
+{
+	int sum = 0, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+
+}
