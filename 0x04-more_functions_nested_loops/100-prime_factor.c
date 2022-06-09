ec6f9aad3c2adca9bfcf5cedf0331220d10eaed1
ofemjohn <ofemjohn@gmail.com>
2022-06-09 05:05:17 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,4 @@
-#include <math.h>
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - factor
@@ -7,24 +6,28 @@
  * Return: 0
  *
  */
-int main(void)
-{
-	long n = 612852475143;
-	int maxPrime = -1;
-	int i;
 
-	for (i = 3; i <= n; i += 2)
+int main(void)
+
+{
+
+	unsigned long int a, i;
+
+
+
+	a = 612852475143;
+
+	for (i = 3; i < (a / 2); i = i + 2)
+
 	{
-		while (n % i == 0)
-		{
-			maxPrime = i;
-			n = n / i;
-		}
+		while ((a % i == 0) && (a != i))
+
+			a = a / i;
 
 	}
-	if (n > 2)
-		maxPrime = n;
-	printf("%d\n", maxPrime);
+
+	printf("%lu\n", a);
+
 	return (0);
 
 }
