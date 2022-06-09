6615ced4fee97a5111dca93e89e08a3c3c6799d4
John Ofem <104776646+ofemjohn@users.noreply.github.com>
2022-06-09 15:37:45 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,10 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
- *
  */
 
-
-
 int main(void)
-
 {
 	int i;
 
@@ -18,5 +13,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
