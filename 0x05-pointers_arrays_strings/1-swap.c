6168e5fe45fd14104aaa46c610192c521348cdf7
ofemjohn <ofemjohn@gmail.com>
2022-06-13 04:59:53 -0700

    #include "holberton.h"
    /**
     * swap_int - Swap the value of two integers
     * @a: Integer a
     * @b: Integer b
     *
     * Return: Nothing
    */
    
    void swap_int(int *a, int *b)
    {
    int c;
    
    c = *a;
    *a = *b;
    *b = c;

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Swap the value of two integers
+ * @a: Integer a
+ * @b: Integer b
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
