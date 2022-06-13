8471386e00a1734f9cc72d3cb310910cca0ae201
ofemjohn <ofemjohn@gmail.com>
2022-06-13 07:08:10 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * puts_half - printing half a string
+ * @str: that's the string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	char *start = str;
+
+	while (*str)
+	{
+		length++;
+		str++;
+
+		length += 1;
+		start += length / 2;
+
+		while (*start)
+		{
+			_putchar(*(start));
+			start++;
+		}
+		_putchar('\n');
+	}
+}
+
