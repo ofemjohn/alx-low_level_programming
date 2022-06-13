cadade18c9b27b70841a5339514346563477ee3f
ofemjohn <ofemjohn@gmail.com>
2022-06-13 11:05:20 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,53 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
- *_atoi - convert string to int
- * @s: the string
- * Return: int
+ * _atoi - check the code for Holberton School students.
+ * @s: pointer char type
+ * Return: Always 0.
  */
 
 int _atoi(char *s)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int sign = 1;
-	int stop_sign = 0;
-	int stop_num = 0;
-	int ent_num = 0;
-	char number_in_array[100];
+	int negative = 1, i = 0;
+	unsigned int numero = 0;
 
-	while (*(s + i) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) == '+' && stop_sign == 0)
-			sign *= 1;
-		else if (*(s + i) == '-' && stop_sign == 0)
-			sign *= -1;
-		if (*(s + i) >= '0' && *(s + i) <= '9' && stop_num == 0)
-		{
-			number_in_array[j] = (*(s + i) - 48);
-			stop_sign = 1;
-			ent_num = 1;
-			j++;
-		}
-		else
-		{
-			if (ent_num == 1)
-				stop_num = 1;
-			else
-			{
-				number_in_array[j] = '0';
-				number_in_array[1] = '\0';
-			}
-		}
-		i++;
+		if (s[i] == '-')
+			negative *= -1;
+
+		else if (s[i] >= 0 + '0' && s[i] < 10 + '0')
+			numero = numero * 10 + (s[i] - '0');
+
+		else if (s[i - 1] >= 0 + '0' && s[i - 1] < 10 + '0')
+			break;
 	}
-	number_in_array[j] = '0';
-	number_in_array[1] = '\0';
-	{
-		putchar(number_in_array[k]);
-		k++;
-	}
-	return (0);
+
+	return (numero * negative);
 }
+
+
