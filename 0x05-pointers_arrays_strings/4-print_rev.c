c770611af44fa53b90b114f6ac3076ce2b3a0c06
ofemjohn <ofemjohn@gmail.com>
2022-06-13 06:34:25 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: that's the string
+ *
+ * Return: void
+ */
+
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	for (; *s; )
+	{
+		count++;
+		s++;
+	}
+	for (; count; count--)
+	{
+		s--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
