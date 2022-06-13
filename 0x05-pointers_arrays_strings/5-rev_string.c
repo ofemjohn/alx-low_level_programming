56c87ba5d47dc9773355322e072701030bce9656
ofemjohn <ofemjohn@gmail.com>
2022-06-13 06:48:37 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * rev_string - reverse a string
+ * @s: that's the string
+ *
+ * Return: void)
+ *
+ */
+
+void rev_string(char *s)
+{
+	char *start = s;
+	char temp;
+
+	for (; *(s + 1); )
+		s++
+	while (start < s)
+	{
+		temp = *start;
+		*start = *s;
+		*s = temp;
+		s--;
+		start++;
+	}
+
+}
