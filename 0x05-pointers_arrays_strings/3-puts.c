163a63321781d9f85c1228bbb21b1605c1d88324
ofemjohn <ofemjohn@gmail.com>
2022-06-13 06:16:01 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,24 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-
 
 /**
  * main - check the code for Holberton School students.
  *
- *
- *
  * Return: Always 0.
- *
  */
-
 int main(void)
 
 {
 	char *str;
 
 	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-		_puts(str);
+	_puts(str);
 
-		return (0);
-
+	return (0);
 }
