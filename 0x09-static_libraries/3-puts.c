e848c3714a554579227c444d26af598c65cad537
ofemjohn <ofemjohn@gmail.com>
2022-06-24 06:41:23 -0700

    libmya.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string to standard output
+ * @str: is the string
+ *
+ * Return: is void
+ */
+
+void _puts(char *str)
+{
+	while (*str != 0)
+	{
+		_putchar(*str);
+		&*str++;
+	}
+	_putchar('\n');
+}
