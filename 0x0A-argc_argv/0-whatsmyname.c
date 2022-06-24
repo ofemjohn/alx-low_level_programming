129fa6ce766c51f708f5d0f04575023ebfd1fc7e
ofemjohn <ofemjohn@gmail.com>
2022-06-24 12:55:24 -0700

    #include <stdio.h>
    /**
     * main - function
     * @argc: offset count
     * @argv: offset value
     * Return: 0
     */
    
    int main(int argc, char *argv[])
    {
    printf("%s\n", argv[argc - 1]);
    return (0);
    }
    
    clear
    clear
    
    q
    .
    >
    <
    "

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc - 1]);
+
 		return (0);
 }
