1704941c764836217dfb402e764f81c6d7ceefd4
John Ofem <104776646+ofemjohn@users.noreply.github.com>
2022-06-30 22:56:10 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,3 @@
-#include <limit.h>
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,7 +23,6 @@ int find_len(char *str)
 
 	return (len);
 }
-
 /**
  * create_xarray - Creates an array of chars and initializes it with
  *                 the character 'x'. Adds a terminating null byte.
@@ -51,22 +49,19 @@ char *create_xarray(int size)
 
 	return (array);
 }
-
 /**
  * iterate_zeroes - Iterates through a string of numbers containing
  *                  leading zeroes until it hits a non-zero number.
- * @str: The string of numbers to be iterate through.
+ * @str: The string of numbers to be iterated through.
  *
  * Return: A pointer to the next non-zero element.
- */
-char *iterate_zeroes(char *str)
+ */char *iterate_zeroes(char *str)
 {
 	while (*str && *str == '0')
 		str++;
 
 	return (str);
 }
-
 /**
  * get_digit - Converts a digit character to a corresponding int.
  * @c: The character to be converted.
@@ -87,7 +82,6 @@ int get_digit(char c)
 
 	return (digit);
 }
-
 /**
  * get_prod - Multiplies a string of numbers by a single digit.
  * @prod: The buffer to store the result.
@@ -104,8 +98,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 
 	mult_len = find_len(mult) - 1;
 	mult += mult_len;
-
-	while (*prod)
+while (*prod)
 	{
 		*prod = 'x';
 		prod++;
@@ -118,8 +111,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 		*prod = '0';
 		prod--;
 	}
-
-	for (; mult_len >= 0; mult_len--, mult--, prod--)
+for (; mult_len >= 0; mult_len--, mult--, prod--)
 	{
 		if (*mult < '0' || *mult > '9')
 		{
@@ -135,9 +127,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 
 	if (tens)
 		*prod = (tens % 10) + '0';
-}
-
-/**
+}/**
  * add_nums - Adds the numbers stored in two strings.
  * @final_prod: The buffer storing the running final product.
  * @next_prod: The next product to be added.
@@ -162,7 +152,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 
 		next_prod--;
 		next_len--;
-	}
+}
 
 	for (; next_len >= 0 && *next_prod != 'x'; next_len--)
 	{
@@ -178,7 +168,6 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 	if (tens)
 		*final_prod = (tens % 10) + '0';
 }
-
 /**
  * main - Multiplies two positive numbers.
  * @argv: The number of arguments passed to the program.
@@ -208,8 +197,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-
-	size = find_len(argv[1]) + find_len(argv[2]);
+size = find_len(argv[1]) + find_len(argv[2]);
 	final_prod = create_xarray(size + 1);
 	next_prod = create_xarray(size + 1);
 
@@ -225,8 +213,7 @@ int main(int argc, char *argv[])
 			putchar(final_prod[index]);
 	}
 	putchar('\n');
-
-	free(next_prod);
+free(next_prod);
 	free(final_prod);
 
 	return (0);
