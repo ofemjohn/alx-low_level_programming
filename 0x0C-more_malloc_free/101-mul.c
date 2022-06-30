8673d45668e897f6c4486d494cfb3f6519a3af22
ofemjohn <ofemjohn@gmail.com>
2022-06-30 15:07:44 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -99,10 +99,10 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 	mult_len = find_len(mult) - 1;
 	mult += mult_len;
 while (*prod)
-	{
-		*prod = 'x';
-		prod++;
-	}
+{
+	*prod = 'x';
+	prod++;
+}
 
 	prod--;
 
@@ -112,22 +112,21 @@ while (*prod)
 		prod--;
 	}
 for (; mult_len >= 0; mult_len--, mult--, prod--)
+{
+	if (*mult < '0' || *mult > '9')
 	{
-		if (*mult < '0' || *mult > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-		num = (*mult - '0') * digit;
-		num += tens;
-		*prod = (num % 10) + '0';
-		tens = num / 10;
+		printf("Error\n");
+		exit(98);
 	}
-
-	if (tens)
-		*prod = (tens % 10) + '0';
-}/**
+	num = (*mult - '0') * digit;
+	num += tens;
+	prod = (num % 10) + '0';
+	tens = num / 10;
+}
+if (tens)
+	*prod = (tens % 10) + '0';
+}
+/**
  * add_nums - Adds the numbers stored in two strings.
  * @final_prod: The buffer storing the running final product.
  * @next_prod: The next product to be added.
@@ -152,6 +151,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 
 		next_prod--;
 		next_len--;
+	}
 }
 
 	for (; next_len >= 0 && *next_prod != 'x'; next_len--)
